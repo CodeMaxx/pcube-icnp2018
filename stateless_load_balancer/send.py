f38f255f99c918dbf89cbe68c3810cc7f324be61
Akash Trehan <akash.trehan123@gmail.com>
2018-01-29 15:42:00 +0530

    Added stateless load balancer

---
+++ stateless_load_balancer/send.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+# Copyright 2013-present Barefoot Networks, Inc. 
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from scapy.all import sniff, sendp
+from scapy.all import Packet
+from scapy.all import ShortField, IntField, LongField, BitField
+
+import networkx as nx
+
+import sys
+
+class SrcRoute(Packet):
+    name = "SrcRoute"
+    fields_desc = [
+        LongField("preamble", 0),
+        IntField("num_valid", 2)
+    ]
+
+def read_topo():
+    nb_hosts = 0
+    nb_switches = 0
+    links = []
+    with open("topo.txt", "r") as f:
+        line = f.readline()[:-1]
+        w, nb_switches = line.split()
+        assert(w == "switches")
+        line = f.readline()[:-1]
+        w, nb_hosts = line.split()
+        assert(w == "hosts")
+        for line in f:
+            if not f: break
+            a, b = line.split()
+            links.append( (a, b) )
+    return int(nb_hosts), int(nb_switches), links
+
+def main():
+
+    nb_hosts, nb_switches, links = read_topo()
+
+    n = 100
+    while(n):
+        first = None
+        p = SrcRoute(num_valid = 4) / str(n)
+        print p.show()
+        sendp(p, iface = "eth0")
+        n-=1
+
+if __name__ == '__main__':
+    main()
