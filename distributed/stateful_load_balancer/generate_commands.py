4d7380e0fe9e09175088ea396d0cb1f3db570202
Akash Trehan <akash.trehan123@gmail.com>
2018-03-05 23:27:41 +0530

    Script for automated building of commands.txt

---
+++ distributed/stateful_load_balancer/generate_commands.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Copyright 2018-present Akash Trehan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+########################################################
+# 1. generate commands.txt based on threshold
+########################################################
+
+import sys
+
+template = open('commands_template.txt', 'r')
+s = template.read() + "\n"
+template.close()
+
+if len(sys.argv) < 2:
+    print("Format: %s <SERVER_THRESHOLD>" % sys.argv[0])
+    sys.exit()
+
+THRESHOLD = int(sys.argv[1])
+S1PORT = 2
+S2PORT = 3
+
+for s1_load in range(THRESHOLD + 1):
+    for s2_load in range(THRESHOLD + 1):
+        if s1_load == THRESHOLD and s2_load == THRESHOLD:
+            break
+        minload = min(s1_load, s2_load)
+        port = 0
+        if minload == s1_load:
+            port = S1PORT
+        else:
+            port = S2PORT
+        s += "table_add set_server_dest_port_table set_server_dest_port %d %d => %d %d\n" % (s1_load, s2_load, minload, port)
+
+commands = open('commands.txt', 'w')
+commands.write(s)
+commands.close()
